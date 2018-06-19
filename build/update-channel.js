14aa898206c34e5a334c93fd20dc8d3e9692bc94
Connor Peet <connor@peet.io>
2018-06-19 12:28:18 -0700

    test [ci skip]

--- build/update-channel.js
+++ build/update-channel.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const channel = process.env.CDK_CHANNEL || 'beta';
 const original = fs.readFileSync(`${__dirname}/../package.json`, 'utf-8');
 const replaced = original.replace(
-  /cdkreleases\.blob\.core\.windows\.net\/cdk-[a-z]+/,
+  /cdkreleases\.blob\.core\.windows\.net\/cdk-[a-z]+/g,
   `cdkreleases.blob.core.windows.net/cdk-${channel}`
 );
 
