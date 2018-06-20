3db8a2940565fda8f1c04e585257cc1f4479e6fd
Connor Peet <connor@peet.io>
2018-06-20 12:44:02 -0700

    test [ci skip]

--- build/update-checksum.js
+++ build/update-checksum.js
@@ -1,74 +1,89 @@
 const yaml = require('node-yaml');
 const crypto = require('crypto');
+const path = require('path');
 const fs = require('fs');
-const { version } = require('../package.json');
-const sha512ForWin = crypto.createHash('sha512');
-const sha256ForWin = crypto.createHash('sha256');
-const sha512ForMac = crypto.createHash('sha512');
-const sha256ForMac = crypto.createHash('sha256');
-const fdExe = fs.createReadStream(`signed/CDK-${version}.exe`);
-const fdZip = fs.createReadStream(`signed/CDK-${version}.zip`);
-sha512ForWin.setEncoding('binary');
-sha512ForMac.setEncoding('binary');
+const version = require('../package.json').version;
 
-console.log('Reading latest.yml');
-yaml.read('signed/latest.yml', function(err, data) {
-  if (err) {
-    console.log('Failed to read latest.yml');
-    throw err;
-  }
-  console.log('Obtaining SHA512 of signed exe...');
-  fdExe.on('end', function() {
-    sha512ForWin.end();
-    sha256ForWin.end();
-    var newSha512 = Buffer.from(sha512ForWin.read(), 'binary').toString('base64');
-    var newSha256 = Buffer.from(sha256ForWin.read()).toString('hex');
-    data.sha512 = newSha512;
-    data.sha2 = newSha256;
-    data.files.map(function(file) {
-      file.sha512 = newSha512;
-    });
-    console.log('Updating latest.yml...');
-    yaml.write('signed/latest.yml', data, function(err) {
-      if (err) {
-        console.log('Failed to write to latest.yml');
-        throw err;
+function abortOnError(callback) {
+  return (err, data) => {
+    if (!err) {
+      if (callback) {
+        callback(data);
       }
-      console.log('Updated latest.yml with new sha512 and sha256');
-    });
+      return;
+    }
+
+    console.error(err);
+    process.exit(1);
+  };
+}
+
+function rewriteYaml(file, mod, callback) {
+  yaml.read(file, (err, data) => {
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    const output = mod(data) || data;
+    yaml.write(file, output, callback);
   });
+}
 
-  fdExe.pipe(sha512ForWin);
-  fdExe.pipe(sha256ForWin);
-});
+function checksumFile(file, algorithms, callback) {
+  const stream = fs.createReadStream(file);
+  const hashes = algorithms.map(a => crypto.createHash(a));
 
-console.log('Reading latest-mac.yml');
-yaml.read('signed/latest-mac.yml', function(err, data) {
-  if (err) {
-    console.log('Failed to read latest-mac.yml, ignoring...');
-    return;
-  }
-  console.log('Obtaining SHA512 of signed zip...');
-  fdZip.on('end', function() {
-    sha512ForMac.end();
-    sha256ForMac.end();
-    var newSha512 = Buffer.from(sha512ForMac.read(), 'binary').toString('base64');
-    var newSha256 = Buffer.from(sha256ForMac.read()).toString('hex');
-    data.sha512 = newSha512;
-    data.sha2 = newSha256;
-    data.files.map(function(file) {
-      file.sha512 = newSha512;
-    });
-    console.log('Updating latest-mac.yml...');
-    yaml.write('signed/latest-mac.yml', data, function(err) {
-      if (err) {
-        console.log('Failed to write to latest-mac.yml');
-        throw err;
-      }
-      console.log('Updated latest-mac.yml with new sha512 and sha256');
-    });
+  stream.on('data', b => hashes.forEach(h => h.update(b)));
+  stream.on('error', callback);
+  stream.on('end', () => callback(null, hashes.map(h => h.digest())));
+}
+
+function rehashWindows(callback) {
+  checksumFile(`signed/CDK-${version}.exe`, ['sha256', 'sha512'], (err, hashes) => {
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    const sha256 = hashes[0];
+    const sha512 = hashes[1];
+    rewriteYaml(
+      'signed/latest.yml',
+      data => {
+        data.sha512 = sha512.toString('base64');
+        data.sha2 = sha256.toString('hex');
+        data.files.forEach(file => (file.sha512 = data.sha512));
+      },
+      callback
+    );
   });
+}
 
-  fdZip.pipe(sha512ForMac);
-  fdZip.pipe(sha256ForMac);
-});
+function rehashOSX(callback) {
+  checksumFile(`signed/CDK-${version}.zip`, ['sha256', 'sha512'], (err, hashes) => {
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    const sha256 = hashes[0];
+    const sha512 = hashes[1];
+    rewriteYaml(
+      'signed/latest-mac.yml',
+      data => {
+        data.sha512 = sha512.toString('base64');
+        data.sha2 = sha256.toString('hex');
+        data.files
+          .filter(file => file.url.indexOf('.zip') > -1)
+          .forEach(file => (file.sha512 = data.sha512));
+      },
+      callback
+    );
+  });
+}
+
+if (require.main === module) {
+  rehashWindows(abortOnError);
+  rehashOSX(abortOnError);
+}
